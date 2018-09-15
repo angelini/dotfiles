e4f3d76079cd988ef590a7771f175fa9285d59ad
Alex Angelini <alex.louis.angelini@gmail.com>
2018-09-14 19:42:19 -0700

    Fix indentation to 2

--- bashrc
+++ bashrc
@@ -32,8 +32,8 @@ export PYENV_ROOT="${HOME}/.pyenv"
 export PATH="${PYENV_ROOT}/bin:${PATH}"
 
 if which pyenv > /dev/null; then
-   eval "$(pyenv init -)";
-   eval "$(pyenv virtualenv-init -)"
+  eval "$(pyenv init -)";
+  eval "$(pyenv virtualenv-init -)"
 fi
 
 # Rust
--- setup.sh
+++ setup.sh
@@ -4,109 +4,110 @@ echo "* angelini/dotfiles"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [[ "${DEV}" ]]; then
-    echo "* DEV"
-    set -x
-    HOME="${DIR}/dev"
+  echo "* DEV"
+  set -x
+  HOME="${DIR}/dev"
 fi
 
 command_exists() {
-    hash "${1}" 2> /dev/null
+  hash "${1}" 2> /dev/null
 }
 
 if [[ "${OSTYPE}" == "linux-gnu" ]]; then
-    echo "* linux"
-    if command_exists "apt-get"; then
-	echo "* ubuntu"
+  echo "= linux"
+  if command_exists "apt-get"; then
+	echo "- ubuntu"
 	DISTRO="ubuntu"
 	UPDATE="sudo apt-get update -y"
 	INSTALL="sudo apt-get install -y"
 	CHECK="dpkg -l"
-    fi
-    if command_exists "pacman"; then
+  fi
+  if command_exists "pacman"; then
+    echo "- arch"
 	DISTRO="arch"
 	UPDATE="sudo yaourt -Syu --noconfirm"
 	INSTALL="sudo yaourt -Sy --noconfirm"
 	CHECK="yaourt -Qi"
 	if ! command_exists "yaourt"; then
-	    sudo cat <<EOT >> /etc/pacman.conf
+	  sudo cat <<EOT >> /etc/pacman.conf
 [archlinuxfr]
 SigLevel = Never
 Server = http://repo.archlinux.fr/$arch
 EOT
-	    sudo pacman -Syu --noconfirm yaourt
+	  sudo pacman -Syu --noconfirm yaourt
 	fi
-    fi
-    if [[ -z "${UPDATE}" ]]; then
+  fi
+  if [[ -z "${UPDATE}" ]]; then
 	echo "unable to detect version of linux"
 	exit 1
-    fi
+  fi
 elif [[ "${OSTYPE}" == "darwin"* ]]; then
-    echo "* osx"
-    DISTRO="macos"
-    UPDATE="brew update"
-    INSTALL="brew install"
-    CHECK="brew ls --versions"
-    if ! command_exists "brew"; then
-        echo "- installing brew"
-        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" 1> /dev/null
-    fi
+  echo "= osx"
+  DISTRO="macos"
+  UPDATE="brew update"
+  INSTALL="brew install"
+  CHECK="brew ls --versions"
+  if ! command_exists "brew"; then
+    echo "- installing brew"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" 1> /dev/null
+  fi
 fi
 
 link () {
-    local target="${DIR}/${1}"
-    local link="${HOME}/.${2:-$1}"
-    if [[ ! -e "${link}" ]]; then
-        echo "- linking ${target} to ${link}"
-        ln -s "${target}" "${link}"
-    fi
+  local target="${DIR}/${1}"
+  local link="${HOME}/.${2:-$1}"
+  if [[ ! -e "${link}" ]]; then
+    echo "- linking ${target} to ${link}"
+    ln -s "${target}" "${link}"
+  fi
 }
 
 link_config() {
-    link "arch/${1}" "config/${1}"
+  link "arch/${1}" "config/${1}"
 }
 
 install() {
-    local package="${1}"
-    if ! ${CHECK} "${package}" &> /dev/null; then
-        echo "- installing ${package}"
-        ${INSTALL} "${package}" > /dev/null
-    fi
+  local package="${1}"
+  if ! ${CHECK} "${package}" &> /dev/null; then
+    echo "- installing ${package}"
+    ${INSTALL} "${package}" > /dev/null
+  fi
 }
 
 install_pyenv() {
-    local pyenv_dir="${HOME}/.pyenv"
-    if ! command_exists "pyenv"; then
-        echo "- installing pyenv"
-        git clone -q https://github.com/yyuu/pyenv.git "${pyenv_dir}"
-        git clone -q https://github.com/yyuu/pyenv-virtualenv.git "${pyenv_dir}/plugins/pyenv-virtualenv"
-    fi
+  local pyenv_dir="${HOME}/.pyenv"
+  if ! command_exists "pyenv"; then
+    echo "- installing pyenv"
+    git clone -q https://github.com/yyuu/pyenv.git "${pyenv_dir}"
+    git clone -q https://github.com/yyuu/pyenv-virtualenv.git "${pyenv_dir}/plugins/pyenv-virtualenv"
+  fi
 }
 
 install_bash_git_prompt() {
-    if [[ ! -d "./bash-git-prompt" ]]; then
-        echo "- installing bash-git-prompt"
-        git clone -q https://github.com/magicmonty/bash-git-prompt.git
-    fi
-    link "bash-git-prompt"
+  if [[ ! -d "./bash-git-prompt" ]]; then
+    echo "- installing bash-git-prompt"
+    git clone -q https://github.com/magicmonty/bash-git-prompt.git
+  fi
+  link "bash-git-prompt"
 
-    if [[ ! -f "./git-completion.bash" ]]; then
-        echo "- installing git-bash-completion"
-        curl -O -fsSL https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
-    fi
-    link "git-completion.bash"
+  if [[ ! -f "./git-completion.bash" ]]; then
+    echo "- installing git-bash-completion"
+    curl -O -fsSL https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+  fi
+  link "git-completion.bash"
 }
 
 install_rustup() {
-    if ! command_exists "rustc"; then
-        curl -fsSL https://sh.rustup.rs > /tmp/rustup_install
-        source /tmp/rustup_install
-    fi
+  if ! command_exists "rustc"; then
+    curl -fsSL https://sh.rustup.rs > /tmp/rustup_install
+    source /tmp/rustup_install
+  fi
 }
 
 install_cask() {
-    if ! command_exists "cask"; then
-        curl -fsSL https://raw.githubusercontent.com/cask/cask/master/go | python
-    fi
+  if ! command_exists "cask"; then
+    curl -fsSL https://raw.githubusercontent.com/cask/cask/master/go | python
+  fi
 }
 
 echo "= updating"
@@ -114,26 +115,26 @@ ${UPDATE}
 mkdir -p "${HOME}/bin"
 
 if [[ "${DISTRO}" == "ubuntu" ]]; then
-    echo "= ubuntu specific"
-    rm "${HOME}/.bashrc"
-    install "curl"
-    if ! command_exists "rg"; then
+  echo "= ubuntu specific"
+  rm "${HOME}/.bashrc"
+  install "curl"
+  if ! command_exists "rg"; then
 	curl -O -fsSL https://github.com/BurntSushi/ripgrep/releases/download/0.10.0/ripgrep_0.10.0_amd64.deb
 	${INSTALL} ./ripgrep_0.10.0_amd64.deb
-    fi
-    if ! command_exists "fd"; then
+  fi
+  if ! command_exists "fd"; then
 	curl -O -fsSL https://github.com/sharkdp/fd/releases/download/v7.1.0/fd-musl_7.1.0_amd64.deb
 	${INSTALL} ./fd-musl_7.1.0_amd64.deb
-    fi
+  fi
 fi
 
 if [[ "${DISTRO}" == "arch" ]]; then
-    echo "= arch specific"
-    link_config "dunst"
-    link_config "fontconfig"
-    link_config "i3"
-    link_config "i3status"
-    link_config "termite"
+  echo "= arch specific"
+  link_config "dunst"
+  link_config "fontconfig"
+  link_config "i3"
+  link_config "i3status"
+  link_config "termite"
 fi
 
 echo "= dotfiles"
@@ -164,16 +165,16 @@ echo "= python"
 install_pyenv
 
 if [[ "$(pyenv global)" != "3.7.0" ]]; then
-    pyenv install 3.7.0
-    pyenv global 3.7.0
+  pyenv install 3.7.0
+  pyenv global 3.7.0
 fi
 
 echo "= rust"
 install_rustup
 
 if [[ "${OSTYPE}" == "linux-gnu" ]]; then
-    echo "= clang"
-    install "clang"
+  echo "= clang"
+  install "clang"
 fi
 
 echo "= emacs-config"
